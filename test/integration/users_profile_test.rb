2ce88b333283e810be0e951fc86714c8de24006c
Rubio Kishigami <rubiokishi@gmail.com>
2019-04-25 19:40:20 +0900

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -17,5 +17,16 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.each do |micropost|
       assert_match micropost.content, response.body
     end
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
+
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+
+
 end
